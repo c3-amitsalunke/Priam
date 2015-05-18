96f713471eaaa7afe83bafd599bcf9ab7e016d62
Amit Salunke <salunkea@gmail.com>
2015-05-18 16:19:33 -0700

    use IAM Credentials

--- priam/src/main/java/com/netflix/priam/defaultimpl/PriamGuiceModule.java
+++ priam/src/main/java/com/netflix/priam/defaultimpl/PriamGuiceModule.java
@@ -15,6 +15,7 @@
  */
 package com.netflix.priam.defaultimpl;
 
+import com.netflix.priam.aws.IAMCredential;
 import org.quartz.SchedulerFactory;
 import org.quartz.impl.StdSchedulerFactory;
 
@@ -42,7 +43,7 @@ public class PriamGuiceModule extends AbstractModule
         bind(IBackupFileSystem.class).annotatedWith(Names.named("backup")).to(S3FileSystem.class);
         bind(IBackupFileSystem.class).annotatedWith(Names.named("incr_restore")).to(S3FileSystem.class);
         bind(IBackupFileSystem.class).annotatedWith(Names.named("backup_status")).to(S3FileSystem.class);
-        bind(ICredential.class).to(ClearCredential.class);
+        bind(ICredential.class).to(IAMCredential.class);
         bind(IDeadTokenRetriever.class).to(DeadTokenRetriever.class);
         bind(IPreGeneratedTokenRetriever.class).to(PreGeneratedTokenRetriever.class);
         bind(INewTokenRetriever.class).to(NewTokenRetriever.class);
